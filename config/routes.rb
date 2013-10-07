d8174197edeb7c9afb926477d35a1538a9259f44
gabriel <gabriel.sprenger@gmail.com>
2013-10-07 19:17:58 +0200

    Added index views for main controllers. Edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,31 @@
 Sustainabilitychecker::Application.routes.draw do
-  get "home/index"
+  get "next/index"
+  get "unsustainable/index"
+  # ROOT
   root 'home#index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # HOME CONTROLLER
+  get 'home' => 'home#index'
+  get 'home/*i' => 'home#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # DEMAND CONTROLLER
+  get 'demand' => 'demand#index'
+  get 'demand/*i' => 'demand#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # SUPPLY CONTROLLER
+  get 'supply' => 'supply#index'
+  get 'supply/*i' => 'supply#index'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # SUDOKU CONTROLLER
+  get 'sudoku' => 'sudoku#index'
+  get 'sudoku/*i' => 'sudoku#index'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # UNSUSTAINABLE CONTROLLER
+  get 'unsustainable' => 'unsustainable#index'
+  get 'unsustainable/*i' => 'unsustainable#index'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # NEXT CONTROLLER
+  get 'next' => 'next#index'
+  get 'next/*i' => 'next#index'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
