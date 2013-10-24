2063c15000e5622d68d3801555e798e8dccb8189
Gabriel Sprenger <gabriel.sprenger@gmail.com>
2013-10-24 20:12:45 +0200

    Added a Helper for Variables that formats data

--- app/helpers/variables_helper.rb
+++ app/helpers/variables_helper.rb
@@ -1,2 +1,41 @@
 module VariablesHelper
+  def collapse_id(lv1)
+    "collapse_" + lv1['name'].downcase
+  end
+
+  def generate_lv3(name, type, values)
+    html = ""
+
+    case type
+    when "radio"
+      html = generate_radio_lv3(name, values)
+    when "sliders"
+    when "sliders100"
+    end
+
+    html
+  end
+
+  def generate_radio_lv3(name, values) 
+    html = 
+    '<div class="row">'
+
+    values.each do |v|
+      name.downcase!
+      vname = v['name'].downcase
+
+      html +=
+      '<div class="col-md-4">' +
+      '  <div class="radio">' +
+      '    <label>' + 
+      '      <img href="#" alt="' + vname + '">' +  
+      '      <input type="radio" name="radio-' + name + '" id="radio-' + name + '-' + vname + '" value="' + vname + '">' + 
+      '    </label>' + 
+      '  </div>' +
+      '</div>'
+    end
+    
+    html += 
+    '</div>'
+  end
 end
