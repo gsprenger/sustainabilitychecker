131bf548279f8b049c04fa849d22ca02d150b06d
Gabriel Sprenger <gabriel.sprenger@gmail.com>
2013-11-07 05:58:40 -0500

    Corrected typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Sustainabilitychecker::Application.routes.draw do
   root "static_pages#home"
 
   # Experience
-  get "begin" => "experience#begin"
+  get "begin" => "experiment#begin"
   # Phase One
   get "demand" => "variables#demand"
   get "supply" => "variables#supply"
