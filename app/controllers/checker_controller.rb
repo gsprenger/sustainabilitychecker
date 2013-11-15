eb1593ea8dba75024d310b3333dcd2f3f49dace6
Gabriel Sprenger <gabriel.sprenger@gmail.com>
2013-11-15 22:17:12 +0100

    Fix #9. Added Save system for Experiments in Chcker controller as well as API methods

--- app/controllers/checker_controller.rb
+++ app/controllers/checker_controller.rb
@@ -5,13 +5,44 @@ class CheckerController < ApplicationController
   # Logic methods:
   ###  
   # Initialize is not used because we need the session context
-  def init
-    # determine session
-    @session = retrieve_session
+  def init_view
+    @experiment = log_in
   end
 
-  def retrieve_session
-    session[:uid] || 'None'
+  def log_in
+    if session[:uid] 
+      e = Experiment.find_by id: session[:uid]
+    end
+    # if no session or cant find the one recorded in the session: check cookies
+    unless session[:uid] && e
+      if cookies[:uid]
+        e = Experiment.find_by id: cookies[:uid]
+      end 
+      unless cookies[:uid] && e
+        e = Experiment.new(json: '{}')
+        e.save
+      end
+    end
+
+    cookies.permanent[:uid] = session[:uid] = e.id
+    return e
+  end
+
+  def get_experiment
+    e = Experiment.find_by id: params[:id]
+    respond_to do |format|
+      format.js { render json: e }
+    end
+  end
+
+  def save_experiment
+    if e = Experiment.find_by id: params[:id]
+      e.json = params[:json]
+      success = e.save
+    end
+    respond_to do |format|
+      format.js { render json: success }
+    end
   end
   
   # Looks for all cards in the cards folder and return them
@@ -36,9 +67,10 @@ class CheckerController < ApplicationController
   # View methods: Variables, Check
   ###
   def variables
-    @nav = %w[demographics diet households services density agriculture energy industrialization]
+    init_view
   end
 
   def check
+    init_view
   end
 end
