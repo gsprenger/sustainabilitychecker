1cb2d41b4dadead83d5ac7fdd13d63fe990a3abe
Gabriel <gabriel.sprenger@gmail.com>
2014-04-03 20:19:24 +0200

    Created level2 and level3 JS entry points

---
+++ app/assets/javascripts/level3.js
@@ -0,0 +1,14 @@
+// This is a manifest file that'll be compiled into checker.js, which will include all the files
+// listed below.
+//
+//= require jquery.ui.slider
+//= require_tree ./checker
+
+var ready = function() {
+  app = App.get()
+  app.setup()
+  app.launchLevel(3)
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
