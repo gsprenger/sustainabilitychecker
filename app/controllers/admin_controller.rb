0209e6785ae59f06a4c39004760ad288c167fabb
Gabriel <gabriel.sprenger@gmail.com>
2014-04-09 14:48:54 +0200

    Changed text in Admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,4 @@
 class AdminController < ApplicationController
-  layout 'static_pages'
   http_basic_authenticate_with :name => "iaste", :password => (ENV['ADMIN_PWD'] || 'dev')
   
   def admin     
